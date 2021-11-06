188ec364d7cb305b77a78f64c8edafe421c0692c
OyebanjiOlawale <88314197+OyebanjiOlawale@users.noreply.github.com>
2021-11-06 14:27:34 +0100

    Update getline.c

--- getline.c
+++ getline.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
-* authors:Amos Mwongela and Oyebanji
+* authors:Amos Mwongela and Oyebanji Olawale Amzat
 * _getline - Read The Input By User From Stdin
 * Return: Input
 *alx-school-- shell
