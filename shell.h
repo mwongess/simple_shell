c93e71a3669ce5132a725ea6dbfae0dfa1f7ebbe
cm-amos <amosmwongelah@gmail.com>
2021-11-06 18:43:48 +0300

    Upload shell.h

--- shell.h
+++ shell.h
@@ -1,51 +1,114 @@
-/**
- * File: shell.h
- * This code defines the header file for shell
- */
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
-#ifndef SHELL
-#define SHELL
+/**###### environ var ######*/
+
+extern char **environ;
+
+/**##### MACROS ######*/
+
+#define BUFSIZE 1024
+#define DELIM " \t\r\n\a"
+#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
+
+/**###### LIBS USED ######*/
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <dirent.h>
-#include <stddef.h>
-#include <errno.h>
 #include <sys/types.h>
+#include <string.h>
 #include <sys/wait.h>
-#define TOKENS_BUFFER_SIZE 64
-#define LINE_SIZE 1024
-#define TOKEN_DELIMITERS " \t\r\n\a"
-extern char **environ;
-/**
- * struct builtins - Has builtins and associated funcs
- * @arg: Builtins name
- * @builtin: Mathcing builtin func
- */
-typedef struct builtins
-{
-	char *arg;
-	void (*builtin)(char **args, char *line, char **env);
-} builtins_t;
-void shell(int ac, char **av, char **env);
-char *_getline(void);
-char **split_line(char *line);
-int execute_prog(char **args, char *line, char **env, int flow);
-int check_for_builtins(char **args, char *line, char **env);
-int launch_prog(char **args);
-void exit_shell(char **args, char *line, char **env);
-void env_shell(char **args, char *line, char **env);
-int _strcmp(char *s1, char *s2);
-char *find_path(char *args, char *tmp, char *er);
-char *search_cwd(char *filename, char *er);
-int bridge(char *check, char **args);
-void prompt(void);
-int builtins_checker(char **args);
-char *save_path(char *tmp, char *path);
-char *read_dir(char *er, struct dirent *s, char *fi, int l, char *p, char *t);
-char *_getenv(char *env);
-char *_strstr(char *haystack, char *needle);
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
+
+
+
+
+
+/**###### STRING FUNCTION ######*/
+
+char *_strtok(char *str, const char *tok);
+unsigned int check_delim(char c, const char *str);
+char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
+int _putchar(char c);
+int _atoi(char *s);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+void array_rev(char *arr, int len);
+int intlen(int num);
+char *_itoa(unsigned int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strdup(char *str);
+
+/**###### MEMORIE  MANGMENT ####*/
+
+void free_env(char **env);
+void *fill_an_array(void *a, int el, unsigned int len);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void *_calloc(unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void free_all(char **input, char *line);
+
+/**###### INPUT Function ######*/
+
+void prompt(void);
+void signal_to_handel(int sig);
+char *_getline(void);
+
+/** ###### Command parser and extractor ###*/
+
+int path_cmd(char **line);
+char *_getenv(char *name);
+char **parse_cmd(char *cmd);
+int handle_builtin(char **cmd, int er);
+void read_file(char *filename, char **argv);
+char *build(char *token, char *value);
+int check_builtin(char **cmd);
+void creat_envi(char **envi);
+int check_cmd(char **tokens, char *line, int count, char **argv);
+void treat_file(char *line, int counter, FILE *fd, char **argv);
+void exit_bul_for_file(char **cmd, char *line, FILE *fd);
+
+/** ####BUL FUNC #####*/
+
+void hashtag_handle(char *buff);
+int history(char *input);
+int history_dis(char **cmd, int er);
+int dis_env(char **cmd, int er);
+int change_dir(char **cmd, int er);
+int display_help(char **cmd, int er);
+int echo_bul(char **cmd, int er);
+void  exit_bul(char **cmd, char *input, char **argv, int c);
+int print_echo(char **cmd);
+
+/** ####error handle and Printer ####*/
+void print_number(unsigned int n);
+void print_number_in(int n);
+void print_error(char *line, int c, char **argv);
+void _prerror(char **argv, int c, char **cmd);
+
+
+/**
+ * authors:gideon mokaya and gedeon obae
+ * struct bulltin - contain bultin to handle and function to excute
+ * @command:pointer to char
+ * @fun:fun to excute when bultin true
+ */
+
+typedef struct  bulltin
+{
+	char *command;
+	int (*fun)(char **line, int er);
+} bul_t;
+
 #endif
+
