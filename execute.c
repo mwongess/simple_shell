11e39af9b65b8ca24a087cea32850964d2c1f27c
cm-amos <amosmwongelah@gmail.com>
2021-11-06 15:09:23 +0300

    Upload execute.c

--- execute.c
+++ execute.c
@@ -1,24 +1,89 @@
 #include "shell.h"
 
 /**
- * execute_prog - Executes binary or builtin
- * @args: Argument buffer
- * @line: Line buffer
- * @env: Environment
- * @flow: Helps decide what code to run
- * Return: 1 if success
+ * authors:Amos Mwongelaa and Oyebanji
+ * handle_builtin - Handle Builtin Command
+ * @cmd: Parsed Command
+ * @er:statue of last Excute
+ * Return: -1 Fail 0 Succes (Return :Excute Builtin)
  */
-int execute_prog(char **args, char *line, char **env, int flow)
+
+int handle_builtin(char **cmd, int er)
+{
+	 bul_t bil[] = {
+		{"cd", change_dir},
+		{"env", dis_env},
+		{"help", display_help},
+		{"echo", echo_bul},
+		{"history", history_dis},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while ((bil + i)->command)
+	{
+		if (_strcmp(cmd[0], (bil + i)->command) == 0)
+		{
+			return ((bil + i)->fun(cmd, er));
+		}
+		i++;
+	}
+	return (-1);
+}
+/**
+ * check_cmd - Excute Simple Shell Command (Fork,Wait,Excute)
+ *
+ * @cmd:Parsed Command
+ * @input: User Input
+ * @c:Shell Excution Time Case of Command Not Found
+ * @argv:Program Name
+ * Return: 1 Case Command Null -1 Wrong Command 0 Command Excuted
+ */
+int check_cmd(char **cmd, char *input, int c, char **argv)
 {
 	int status;
+	pid_t pid;
 
-	if (args[0] == NULL)
-		return (1);
-	if (flow == 1)
+	if (*cmd == NULL)
 	{
-		if (check_for_builtins(args, line, env) == 1)
-			return (1);
+		return (-1);
 	}
-	status = launch_prog(args);
-	return (status);
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error");
+		return (-1);
+	}
+
+	if (pid == 0)
+	{
+		if (_strncmp(*cmd, "./", 2) != 0 && _strncmp(*cmd, "/", 1) != 0)
+		{
+			path_cmd(cmd);
+		}
+
+		if (execve(*cmd, cmd, environ) == -1)
+		{
+			print_error(cmd[0], c, argv);
+			free(input);
+			free(cmd);
+			exit(EXIT_FAILURE);
+		}
+		return (EXIT_SUCCESS);
+	}
+	wait(&status);
+	return (0);
+}
+/**
+ * signal_to_handel - Handle ^C
+ * @sig:Captured Signal
+ * Return: Void
+ */
+void signal_to_handel(int sig)
+{
+	if (sig == SIGINT)
+	{
+		PRINTER("\n$ ");
+	}
 }
